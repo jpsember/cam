412f084579743a16c0dbea44b83064da71897b09
Jeff Sember <jpsember@gmail.com>
2015-04-08 11:14:50 -0700

    Issue #24: added BitmapTools class

--- app/src/main/java/com/js/camera/MainActivity.java
+++ app/src/main/java/com/js/camera/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends Activity {
           throw new UnsupportedOperationException("Unsupported preview image format: " + properties.format());
 
         int rgba[] = new int[previewSize.x * previewSize.y * 2];
-        decodeYUV420SP(rgba, data, previewSize.x, previewSize.y);
+        BitmapTools.decodeYUV420SP(rgba, data, previewSize.x, previewSize.y);
 
         Bitmap bitmap = Bitmap.createBitmap(rgba, previewSize.x, previewSize.y, Bitmap.Config.ARGB_8888);
 
@@ -100,47 +100,6 @@ public class MainActivity extends Activity {
       private int mCounter;
     });
   }
-
-  /**
-   * Decode YUV 4:2:0 bitmap to ARGB_8888 format.
-   * Note that a (fully opaque) alpha channel is added;
-   * if we omit this, I can't then see how to create a Bitmap from the result
-   */
-  private static void decodeYUV420SP(int[] argb, byte[] yuv420sp, int width, int height) {
-    final int frameSize = width * height;
-
-    int uvpBase = frameSize;
-    for (int j = 0, yp = 0; j < height; j++) {
-      int u = 0;
-      int v = 0;
-      int uvp = uvpBase;
-      for (int i = 0; i < width; i++, yp++) {
-        int y = (0xff & ((int) yuv420sp[yp])) - 16;
-        if (y < 0) y = 0;
-        if ((i & 1) == 0) {
-          v = (0xff & yuv420sp[uvp++]) - 128;
-          u = (0xff & yuv420sp[uvp++]) - 128;
-        }
-
-        int y1192 = 1192 * y;
-        int r = (y1192 + 1634 * v);
-        int g = (y1192 - 833 * v - 400 * u);
-        int b = (y1192 + 2066 * u);
-
-        if (r < 0) r = 0;
-        else if (r > 262143) r = 262143;
-        if (g < 0) g = 0;
-        else if (g > 262143) g = 262143;
-        if (b < 0) b = 0;
-        else if (b > 262143) b = 262143;
-
-        argb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
-      }
-      if ((j & 1) != 0)
-        uvpBase += width;
-    }
-  }
-
   private final static int BGND_COLOR = Color.DKGRAY;
 
   private View buildContentView() {
