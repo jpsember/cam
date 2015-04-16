4f442b43d8c2210650708b54bee481cd03cb0f16
Jeff Sember <jpsember@gmail.com>
2015-04-16 16:18:37 -0700

    Issue #35: added GridView for album, with some test images

--- app/src/main/java/com/js/camera/AlbumActivity.java
+++ app/src/main/java/com/js/camera/AlbumActivity.java
@@ -1,19 +1,27 @@
 package com.js.camera;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.js.android.AppPreferences;
-import com.js.android.UITools;
 import com.js.camera.camera.R;
 
+import static com.js.basic.Tools.*;
+
 import static com.js.android.AndroidTools.doNothingAndroid;
-import static com.js.basic.Tools.startApp;
 
 public class AlbumActivity extends Activity {
 
@@ -61,10 +69,75 @@ public class AlbumActivity extends Activity {
   }
 
   private View buildContentView() {
-
-    View view = new View(this);
-    view.setBackgroundColor(UITools.debugColor());
-    return view;
+    return buildGridView();
   }
 
+  public View buildGridView() {
+    GridView v = new GridView(this);
+
+    // Use density pixels here
+    v.setColumnWidth(300);
+    v.setNumColumns(GridView.AUTO_FIT);
+    v.setVerticalSpacing(10);
+    v.setHorizontalSpacing(10);
+    v.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+    v.setGravity(Gravity.CENTER);
+
+    v.setAdapter(new ImageAdapter(this));
+
+    v.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      public void onItemClick(AdapterView<?> parent, View v,
+                              int position, long id) {
+        Toast.makeText(AlbumActivity.this, "" + position,
+            Toast.LENGTH_SHORT).show();
+      }
+    });
+    return v;
+  }
+
+
+  private static class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+      mContext = c;
+    }
+
+    public int getCount() {
+      return 150;
+    }
+
+    public Object getItem(int position) {
+      return null;
+    }
+
+    public long getItemId(int position) {
+      return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+      ImageView imageView;
+      if (convertView == null) {
+        // if it's not recycled, initialize some attributes
+        imageView = new ImageView(mContext);
+        imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8, 8, 8, 8);
+      } else {
+        imageView = (ImageView) convertView;
+      }
+
+      imageView.setImageResource(getThumbId(position));
+      return imageView;
+    }
+
+    private int getThumbId(int position) {
+      return mThumbIds[myMod(position, mThumbIds.length)];
+    }
+
+    private int[] mThumbIds = {
+        R.drawable.ic_launcher, R.drawable.round_button, R.drawable.ic_launcher,
+    };
+  }
 }
