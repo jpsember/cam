9039042e8401a56642bd940f093348085b748809
Jeff Sember <jpsember@gmail.com>
2015-04-30 12:45:00 -0700

    Issue #58: added transform, and two separate image views

--- app/src/main/java/com/js/camera/PicassoDemoActivity.java
+++ app/src/main/java/com/js/camera/PicassoDemoActivity.java
@@ -1,6 +1,10 @@
 package com.js.camera;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +13,7 @@ import android.widget.LinearLayout;
 
 import com.js.android.UITools;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.io.File;
 import java.util.List;
@@ -34,50 +39,64 @@ public class PicassoDemoActivity extends Activity implements Observer {
   }
 
   private View buildContentView() {
-    LinearLayout layout = UITools.linearLayout(this, true);
+    LinearLayout outer = UITools.linearLayout(this, false);
 
-    mImageView = new ImageView(this);
-    mImageView.setBackgroundColor(UITools.debugColor());
-    layout.addView(mImageView, UITools.layoutParams(layout, 1));
 
-    LinearLayout buttonPanel = UITools.linearLayout(this, false);
-    layout.addView(buttonPanel, UITools.layoutParams(layout, 0));
+    for (int pass = 0; pass < 2; pass++) {
+      final boolean parity = pass == 1;
+      LinearLayout layout = UITools.linearLayout(this, true);
+      outer.addView(layout, UITools.layoutParams(outer, 1));
 
-    Button b = new Button(this);
-    b.setText("<<<");
-    buttonPanel.addView(b, UITools.layoutParams(buttonPanel, 1));
-    b.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (mPhotos == null) return;
-        mPhotoIndex = myMod(mPhotoIndex - 1, mPhotos.size());
-        loadImage(mPhotos.get(mPhotoIndex));
-      }
-    });
-    b = new Button(this);
-    b.setText(">>>");
-    buttonPanel.addView(b, UITools.layoutParams(buttonPanel, 1));
-    b.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (mPhotos == null) return;
-        mPhotoIndex = myMod(mPhotoIndex + 1, mPhotos.size());
-        loadImage(mPhotos.get(mPhotoIndex));
-      }
-    });
+      ImageView imageView = new ImageView(this);
+      if (pass == 0) mImageView1 = imageView;
+      else
+        mImageView2 = imageView;
+      imageView.setBackgroundColor(UITools.debugColor());
+      layout.addView(imageView, UITools.layoutParams(layout, 1));
 
-    return layout;
+      LinearLayout buttonPanel = UITools.linearLayout(this, false);
+      layout.addView(buttonPanel, UITools.layoutParams(layout, 0));
+
+      Button b = new Button(this);
+      b.setText("<<<");
+      buttonPanel.addView(b, UITools.layoutParams(buttonPanel, 1));
+      b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          if (mPhotos == null) return;
+          mPhotoIndex = myMod(mPhotoIndex - 1, mPhotos.size());
+          loadImage(parity, mPhotos.get(mPhotoIndex));
+        }
+      });
+      b = new Button(this);
+      b.setText(">>>");
+      buttonPanel.addView(b, UITools.layoutParams(buttonPanel, 1));
+      b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          if (mPhotos == null) return;
+          mPhotoIndex = myMod(mPhotoIndex + 1, mPhotos.size());
+          loadImage(parity, mPhotos.get(mPhotoIndex));
+        }
+      });
+    }
+
+    return outer;
   }
 
-  private void loadImage(PhotoInfo photo) {
+  private void loadImage(boolean parity, PhotoInfo photo) {
+    pr("loadImage, thread " + nameOf(Thread.currentThread()));
     File file = mPhotoFile.tempGetPhotoPath(photo.getId());
-    Picasso.with(this).load(file).into(mImageView, new EmptyCallback() {
-      @Override
-      public void onSuccess() {
-        // Index 0 is the image view.
+    buildTransforms();
+    Picasso.with(this).load(file).transform(parity ? mTrans2 : mTrans1).into(
+        parity ? mImageView2 : mImageView1,
+        new EmptyCallback() {
+          @Override
+          public void onSuccess() {
+            // Index 0 is the image view.
 //            animator.setDisplayedChild(0);
-      }
-    });
+          }
+        });
   }
 
   @Override
@@ -98,7 +117,51 @@ public class PicassoDemoActivity extends Activity implements Observer {
     }
   }
 
-  private ImageView mImageView;
+  private void buildTransforms() {
+    if (mTrans1 != null) return;
+    mTrans1 = new Transformation() {
+      @Override
+      public Bitmap transform(Bitmap bitmap) {
+        Bitmap bitmap2 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        bitmap.recycle();
+        Canvas canvas = new Canvas(bitmap2);
+        Paint p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(Color.GREEN);
+        canvas.drawRect(50, 50, 200, 200, p);
+        return bitmap2;
+      }
+
+      @Override
+      public String key() {
+        return "1";
+      }
+    };
+    mTrans2 = new Transformation() {
+      @Override
+      public Bitmap transform(Bitmap bitmap) {
+        pr("transform, thread " + nameOf(Thread.currentThread()));
+        sleepFor(this == mTrans2 ? 250 : 10);
+        Bitmap bitmap2 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        bitmap.recycle();
+        Canvas canvas = new Canvas(bitmap2);
+        Paint p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(Color.RED);
+        canvas.drawRect(50, 50, 200, 200, p);
+        return bitmap2;
+      }
+
+      @Override
+      public String key() {
+        return "2";
+      }
+    };
+
+  }
+
+  private Transformation mTrans1, mTrans2;
+  private ImageView mImageView1, mImageView2;
   private PhotoFile mPhotoFile;
   private List<PhotoInfo> mPhotos;
   private int mPhotoIndex;
